3fc71aba2f52649db8ed2530e51e7cefbb51001d
Chahkar <aziz.chahkar@accenture.com>
2018-09-11 16:38:58 +0200

    add sample table of data in home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import data from "./data.json";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home Component HERE</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div class="col-lg-8 mx-auto">
+          <h2>Groups</h2>
+          <br />
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(function(group) {
+                return (
+                  <tr>
+                    <th scope="row">
+                      <a href="#">{group.title}</a>
+                    </th>
+                    <td>{group.desc}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
