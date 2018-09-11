b6f2e5819381cd2bf5b4dc4ba3b645f0adc6c953
Chahkar <aziz.chahkar@accenture.com>
2018-09-11 15:41:12 +0200

    I'm sorry

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Home from './components/home/home';
+
+const element = <Home></Home>;
+
+ReactDOM.render(element,document.getElementById("root"));
